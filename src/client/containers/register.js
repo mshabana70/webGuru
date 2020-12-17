7069935587147985f4c02a4943931e6d0594f2e1
Mahmoud Shabana <mshabana5656@gmail.com>
2020-12-17 12:31:41 -0500

    test server requests

--- src/client/containers/register.js
+++ src/client/containers/register.js
@@ -1,1 +1,21 @@
 <h1>register</h1>;
+
+
+export default function Register() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    function signUp() {
+        if (email.match(!emailAn) && password.match(!passwordAn)) {
+            
+            console.log("sign up Admin");
+            return true;
+        } else if (email.match(emailSt) && password.match(passwordSt)) {
+            console.log("Great Success Student");
+            return true;
+        } else {
+            console.log("No Success");
+            return false;
+        }
+    }
